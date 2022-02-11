013d1045ca3c8a5a6a12a28af43b279d9dca9fb0
Amr <amralmoallim@gmail.com>
2022-02-10 18:02:59 -0800

    added interface package

---
+++ vehicle_autonomy/propbot_interface/src/wheel_speed_publisher.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python 
+import rospy 
+from std_msgs.msg import Int8
+
+# Initialize the node with rospy 
+rospy.init_node('propbot_interface') 
+
+# Create publisher 
+pub_l = rospy.Publisher("/left_wheel",Int8,queue_size=1) 
+pub_r = rospy.Publisher("/right_wheel",Int8,queue_size=1) 
+
+
+while not rospy.is_shutdown():
+	print("insert left/right wheel speeds: ")
+	wheel_input = raw_input() #space seperated left right
+	pub_l.publish(wheel_input.split()[0])
+	pub_r.publish(wheel_input.split()[1])	
+	sleep(1)
+	
