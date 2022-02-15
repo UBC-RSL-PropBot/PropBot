2988fb44dbb3649eabbb4bdc297f245dfd8a2cab
Amr <amralmoallim@gmail.com>
2022-02-15 01:56:21 -0800

    changed baud rate

--- vehicle_autonomy/propbot_interface/src/wheel_speed_publisher.py
+++ vehicle_autonomy/propbot_interface/src/wheel_speed_publisher.py
@@ -14,7 +14,7 @@ pub_r = rospy.Publisher("/right_wheel",Int8,queue_size=1)
 while not rospy.is_shutdown():
 	print("insert left/right wheel speeds: ")
 	wheel_input = raw_input() #space seperated left right
-	pub_l.publish(wheel_input.split()[0])
-	pub_r.publish(wheel_input.split()[1])	
+	pub_l.publish(int(wheel_input.split()[0]))
+	pub_r.publish(int(wheel_input.split()[1]))	
 	sleep(1)
 	
