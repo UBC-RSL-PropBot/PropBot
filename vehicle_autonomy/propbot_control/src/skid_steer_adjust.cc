85cf90a31dbdcee58bded34f52ecc01fc555d1f6
Amr <amralmoallim@gmail.com>
2022-04-14 06:22:54 -0700

    added skid steer adjustor

---
+++ vehicle_autonomy/propbot_control/src/skid_steer_adjust.cc
@@ -0,0 +1,70 @@
+#include <string>
+
+#include "gazebo_msgs/GetJointProperties.h"
+#include <ros/package.h>
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include <std_msgs/Float32.h>
+
+#include <ros/console.h>
+
+
+
+geometry_msgs::Twist latest_cmd;
+
+void cmdVelCallback(const geometry_msgs::Twist & msg)
+{
+   latest_cmd = msg;
+}
+
+/* Publish wheel speed from gazebo
+ *
+ * This node calls the ---- service from gazebo and publishes wheel speeds extracted from it
+ * 
+ */
+int main(int argc, char** argv) {
+  // Initiation node called send mission
+  ros::init(argc, argv, "skid_steer_adjustor");
+  ros::NodeHandle nh;  
+
+  // Create publishers
+  ros::Publisher cmd_vel_pub = 
+      nh.advertise<geometry_msgs::Twist>(
+          "cmd_vel", 100);
+
+  ros::Publisher right_wheel_pub = 
+      nh.advertise<std_msgs::Float32>(
+          "right_wheel", 100);
+
+  ros::Publisher left_wheel_pub = 
+      nh.advertise<std_msgs::Float32>(
+          "left_wheel", 100);
+
+  ros::Subscriber sub = nh.subscribe("/move_base/cmd_vel", 10, cmdVelCallback);
+
+  ros::Rate loop_rate(50);
+
+
+  while (nh.ok()) {
+    
+    cmd_vel_pub.publish(latest_cmd);
+
+    std_msgs::Float32 lw;
+    std_msgs::Float32 rw;  
+
+
+    lw.data = 1;
+    rw.data = -1;
+
+
+    // Publish pose
+    left_wheel_pub.publish(lw);
+    right_wheel_pub.publish(rw);
+
+
+    // Sleep
+    loop_rate.sleep();
+  }
+  ros::shutdown();
+  return 0;
+}
