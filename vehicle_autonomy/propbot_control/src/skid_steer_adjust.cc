bcff705fe5b180a78584557b8d7333d647891980
Amr <amralmoallim@gmail.com>
2022-04-14 08:21:56 -0700

    fixed

--- vehicle_autonomy/propbot_control/src/skid_steer_adjust.cc
+++ vehicle_autonomy/propbot_control/src/skid_steer_adjust.cc
@@ -10,11 +10,14 @@
 
 
 
-geometry_msgs::Twist latest_cmd;
+geometry_msgs::Twist latest_rcv_cmd;
 
-void cmdVelCallback(const geometry_msgs::Twist & msg)
+void cmdVelCallback(const geometry_msgs::Twist::ConstPtr & msg)
 {
-   latest_cmd = msg;
+   latest_rcv_cmd.linear.x = msg->linear.x;
+   latest_rcv_cmd.angular.z = msg->angular.z;
+   std::cout<<"im here"<<std::endl;
+//   latest_rcv_cmd = msg;
 }
 
 /* Publish wheel speed from gazebo
@@ -25,46 +28,59 @@ void cmdVelCallback(const geometry_msgs::Twist & msg)
 int main(int argc, char** argv) {
   // Initiation node called send mission
   ros::init(argc, argv, "skid_steer_adjustor");
-  ros::NodeHandle nh;  
+  ros::NodeHandle nh;
 
   // Create publishers
-  ros::Publisher cmd_vel_pub = 
+  ros::Publisher cmd_vel_pub =
       nh.advertise<geometry_msgs::Twist>(
           "cmd_vel", 100);
 
-  ros::Publisher right_wheel_pub = 
+  ros::Publisher right_wheel_pub =
       nh.advertise<std_msgs::Float32>(
           "right_wheel", 100);
 
-  ros::Publisher left_wheel_pub = 
+  ros::Publisher left_wheel_pub =
       nh.advertise<std_msgs::Float32>(
           "left_wheel", 100);
 
   ros::Subscriber sub = nh.subscribe("/move_base/cmd_vel", 10, cmdVelCallback);
 
-  ros::Rate loop_rate(50);
+  ros::Rate loop_rate(10);
+  geometry_msgs::Twist latest_send_cmd;
 
 
   while (nh.ok()) {
-    
-    cmd_vel_pub.publish(latest_cmd);
+
+   //latest_rcv_cmd.linear.x = -0.2;
+   //latest_rcv_cmd.angular.z = -0.1;
+   
+   cmd_vel_pub.publish(latest_rcv_cmd);
 
     std_msgs::Float32 lw;
-    std_msgs::Float32 rw;  
-
-
+    std_msgs::Float32 rw;
     lw.data = 1;
     rw.data = -1;
 
+    if(std::abs(latest_rcv_cmd.angular.z) > 0.1 ){
+        latest_sent_cmd.linear.x = 0.0;
+        latest_sent_cmd.angular.z = latest_rcv_cmd.angular.z;
 
+    } else{
+        latest_sent_cmd.linear.x = latest_rcv_cmd.linear.x;
+        latest_sent_cmd.angular.z = 0.0;
+
+    }
     // Publish pose
     left_wheel_pub.publish(lw);
     right_wheel_pub.publish(rw);
-
+    cmd_vel_pub.publish(latest_rcv_cmd);
 
     // Sleep
+    ros::spinOnce();
     loop_rate.sleep();
   }
   ros::shutdown();
   return 0;
 }
+                                                                                                                                                                                                 78,1          Bot
+
